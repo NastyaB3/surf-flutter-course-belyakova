e08c7de40711eda67fdb8db817bc47a75ca2c65b
anastasia <komarovaanastasia3@gmail.com>
2021-03-27 16:07:06 +0300

    Задание_4.4.2

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -9,12 +9,48 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-       title: Text('Список интересных мест',),
-     ),
-      body: Center(
-        child: Column(
+      appBar: AppBar(
+        centerTitle: false,
+        backgroundColor: Colors.transparent,
+        toolbarHeight: 120,
+        elevation: 0,
+        title: Container(
+          child: RichText(
+            maxLines: 2,
+            text: TextSpan(
+                text: 'C',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green,
+                ),
+                children: [
+                  TextSpan(
+                      text: 'писок',
+                      style: TextStyle(
+                        color: Color(0xff3b3f59),
+                      ),
+                      children: [
+                        TextSpan(
+                            text: '\nи',
+                            style: TextStyle(
+                              color: Colors.yellow,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: 'нтересных мест',
+                                style: TextStyle(
+                                  color: Color(0xff3b3f59),
+                                ),
+                              ),
+                            ]),
+                      ]),
+                ]),
+          ),
         ),
+      ),
+      body: Center(
+        child: Column(),
       ),
     );
   }
