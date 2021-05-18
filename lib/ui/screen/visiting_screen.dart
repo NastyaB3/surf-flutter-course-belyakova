cd4da2983329dccbf18ed50426b7f97bced12477
anastasia <komarovaanastasia3@gmail.com>
2021-05-18 15:58:27 +0300

    Сортировка карточек "хочу посетить"/ "Посетил" с помощью Draggable

--- lib/ui/screen/visiting_screen.dart
+++ lib/ui/screen/visiting_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:places/domain/sight.dart';
 import 'package:places/ui/res/colors.dart';
-import 'package:places/ui/screen/res/themes.dart';
 import 'package:places/widget/sight_card_visited.dart';
 import 'package:places/widget/sight_card_visiting.dart';
 import '../../mocks.dart';
@@ -17,19 +17,14 @@ class _VisitingScreenState extends State<VisitingScreen> {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Theme
-              .of(context)
-              .backgroundColor,
+          backgroundColor: Theme.of(context).backgroundColor,
           toolbarHeight: 120,
           elevation: 0,
           title: Container(
             child: Center(
               child: Text(
                 'Избранное',
-                style: Theme
-                    .of(context)
-                    .textTheme
-                    .headline6,
+                style: Theme.of(context).textTheme.headline6,
               ),
             ),
           ),
@@ -38,16 +33,12 @@ class _VisitingScreenState extends State<VisitingScreen> {
               margin: EdgeInsets.all(16),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(40),
-                color: Theme
-                    .of(context)
-                    .accentColor,
+                color: Theme.of(context).accentColor,
               ),
               child: TabBar(
                 unselectedLabelColor: ltInactiveBlack,
                 indicator: BoxDecoration(
-                  color: Theme
-                      .of(context)
-                      .primaryColor,
+                  color: Theme.of(context).primaryColor,
                   borderRadius: BorderRadius.circular(40),
                 ),
                 tabs: [
@@ -101,21 +92,47 @@ class _VisitingScreenState extends State<VisitingScreen> {
       return SingleChildScrollView(
         child: Column(children: [
           for (var sight in mocks)
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-              child: AspectRatio(
-                aspectRatio: 3 / 2,
-                child: SightCardVisiting(
-                  sight,
-                  onClose: (sight) {
-                    setState(() {
-                      mocks.remove(sight);
-                    });
-                  },
-                  key: ValueKey('Text'),
+            DragTarget(onWillAccept: (Sight data) {
+              return true;
+            }, onAccept: (Sight data) {
+              setState(() {
+                int indexSight = mocks.indexOf(sight);
+                int indexData = mocks.indexOf(data);
+                mocks[indexSight] = data;
+                mocks[indexData] = sight;
+              });
+            }, builder: (context, candidateData, rejectData) {
+              return LongPressDraggable<Sight>(
+                data: sight,
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  child: AspectRatio(
+                    aspectRatio: 3 / 2,
+                    child: SightCardVisiting(
+                      sight,
+                      onClose: (sight) {
+                        setState(() {
+                          mocks.remove(sight);
+                        });
+                      },
+                    ),
+                  ),
                 ),
-              ),
-            ),
+                feedback: Container(
+                  height: MediaQuery.of(context).size.width * 2 / 3,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  child: SightCardVisiting(
+                    sight,
+                    onClose: (sight) {
+                      setState(() {
+                        mocks.remove(sight);
+                      });
+                    },
+                  ),
+                ),
+              );
+            }),
         ]),
       );
     } else {
@@ -130,10 +147,7 @@ class _VisitingScreenState extends State<VisitingScreen> {
           Container(
             child: Text(
               'Пусто',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .headline6,
+              style: Theme.of(context).textTheme.headline6,
             ),
           ),
           SizedBox(
@@ -141,10 +155,7 @@ class _VisitingScreenState extends State<VisitingScreen> {
           ),
           Text(
             "Отмечайте понравившиеся\nместа и они появятся здесь",
-            style: Theme
-                .of(context)
-                .textTheme
-                .bodyText2,
+            style: Theme.of(context).textTheme.bodyText2,
           ),
         ],
       );
@@ -156,21 +167,48 @@ class _VisitingScreenState extends State<VisitingScreen> {
       return SingleChildScrollView(
         child: Column(children: [
           for (var sight in mocks)
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-              child: AspectRatio(
-                aspectRatio: 3 / 2,
-                child: SightCardVisited(
-                  sight,
-                  onClose: (sight) {
-                    setState(() {
-                      mocks.remove(sight);
-                    });
-                  },
-                  key: ValueKey('Text'),
+            DragTarget(onWillAccept: (Sight data) {
+              return true;
+            }, onAccept: (Sight data) {
+              setState(() {
+                int indexData = mocks.indexOf(data);
+                int indexSight = mocks.indexOf(sight);
+                mocks[indexSight] = data;
+                mocks[indexData] = sight;
+              });
+            }, builder: (context, candidateDate, rejectedDate) {
+              return LongPressDraggable(
+                data: sight,
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  child: AspectRatio(
+                    aspectRatio: 3 / 2,
+                    child: SightCardVisited(
+                      sight,
+                      onClose: (sight) {
+                        setState(() {
+                          mocks.remove(sight);
+                        });
+                      },
+                      key: ValueKey('Text'),
+                    ),
+                  ),
                 ),
-              ),
-            ),
+                feedback: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width * 2 / 3,
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  child: SightCardVisited(
+                    sight,
+                    onClose: (sight) {
+                      setState(() {
+                        mocks.remove(sight);
+                      });
+                    },
+                  ),
+                ),
+              );
+            }),
         ]),
       );
     } else {
@@ -185,10 +223,7 @@ class _VisitingScreenState extends State<VisitingScreen> {
           Container(
             child: Text(
               'Пусто',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .headline6,
+              style: Theme.of(context).textTheme.headline6,
             ),
           ),
           SizedBox(
@@ -196,10 +231,7 @@ class _VisitingScreenState extends State<VisitingScreen> {
           ),
           Text(
             "Завершите маршрут,\nчтобы место попало сюда",
-            style: Theme
-                .of(context)
-                .textTheme
-                .bodyText2,
+            style: Theme.of(context).textTheme.bodyText2,
             textAlign: TextAlign.center,
           ),
         ],
